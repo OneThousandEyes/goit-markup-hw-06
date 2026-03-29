71259b3731682548587ee7352a52a2357655c51e
Vitalii Cherpak <46451879+OneThousandEyes@users.noreply.github.com>
2026-03-29 23:36:49 +0300

    add tablet, mobile mode + mobile menu

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -3,6 +3,7 @@
     openMenuBtn: document.querySelector("[data-menu-open]"),
     closeMenuBtn: document.querySelector("[data-menu-close]"),
     menu: document.querySelector("[data-menu]"),
+    menuLinks: document.querySelectorAll(".mobile-menu-nav-link"),
   };
 
   if (!refs.openMenuBtn || !refs.closeMenuBtn || !refs.menu) {
@@ -11,8 +12,15 @@
 
   refs.openMenuBtn.addEventListener("click", toggleMenu);
   refs.closeMenuBtn.addEventListener("click", toggleMenu);
+  refs.menuLinks.forEach((link) => {
+    link.addEventListener("click", closeMenu);
+  });
 
   function toggleMenu() {
     refs.menu.classList.toggle("is-open");
+  }
+
+  function closeMenu() {
+    refs.menu.classList.remove("is-open");
   }
 })();
